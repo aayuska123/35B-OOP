f21945bac86741b2cf993292807fb174e88e1f1e
ayuska <adhikari1@gmail.com>
2024-05-09 14:38:38 +0545

    Output2

---
+++ Week2/Output2.java
@@ -0,0 +1,9 @@
+public class Output2 {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has a main method with { and } braces.\n");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)\n");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+    }
+}
